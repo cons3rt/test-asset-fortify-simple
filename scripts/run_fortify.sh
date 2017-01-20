d5213dc7c10ef19e55c5195e40b25696972a9b95
Joe Yennaco <joe@joeyennaco.com>
2017-01-20 11:34:32 -0500

    Added the simplified version of the run_fortify.sh scan with -autoheap as the memory option.  Also removed the build and scan files from this simple example.

--- scripts/run_fortify.sh
+++ scripts/run_fortify.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# This flow relies on buildId being constant through out configuration and the scan configuration pointing to an output file that report generate can then use
-
 function run_and_check_status() {
     "$@"
     local status=$?
@@ -12,67 +10,56 @@ function run_and_check_status() {
     fi
 }
 
-function replace_tags() {
-    local tag=$1
-    local val=$2
-    local file=$3
-    
-    echo "-- Replacing: ${tag} with ${val} in file: ${file}" 
-
-    sed -i "s@${tag}@${val}@g" "${file}"
-}
-
-function parse_deployment_property() {
-    local property=$1
-    local default=$2
-
-    local retval=$( grep "${property}" ${combinedPropertiesFile} | sed s/.*=// )
-
-    if [ -z "${retval}" ]; then
-        if [ -z "${default}" ]; then
-            exit 1
-        else
-            echo "${default}"
-        fi
-    else
-        echo "${retval}"
-    fi
-}
-
 function fortify() {
-    # Used for debugging of env variables based in by fortify adapter 
+    # Used for debugging of env variables based in by fortify adapter
     printenv > ${logDir}environment_out.log
 
+    echo
     echo -n `date "+%D %H:%M:%S"`
-    echo " : Beginning fortify scan"
-
-    # Note: variables can not be used inside of a Fortify SCA cmd line options file, so replace_me tags are used instead
-    echo "-- Replacing tags in build file: ${buildFile}"
-    replace_tags "REPLACE_BUILD_ID" "${buildId}" "${buildFile}"
-    replace_tags "REPLACE_SOURCE_DIR" "${sourceDir}" "${buildFile}"
-    replace_tags "REPLACE_DEBUG" "${fortifyDebug}" "${buildFile}"
-    replace_tags "REPLACE_BUILD_LOG" "${logDir}${buildId}-build.log" "${buildFile}"
-
+    echo " : Beginning fortify test"
     echo
-    echo "-- Replacing tags in scan file: ${scanFile}"
-    replace_tags "REPLACE_BUILD_ID" "${buildId}" "${scanFile}"
-    replace_tags "REPLACE_DEBUG" "${fortifyDebug}" "${scanFile}"
-    replace_tags "REPLACE_SCAN_OUTPUT_FILE" "${reportDir}${buildId}-scan.fpr" "${scanFile}"
-    replace_tags "REPLACE_SCAN_LOG" "${logDir}${buildId}-scan.log" "${scanFile}"
+    echo -n `date "+%D %H:%M:%S"`
+    echo " : Beginning fortify build of ${sourceDir}"
     echo
 
+    # Begin build process
     run_and_check_status "${sourceanalyzer}" -clean
-    run_and_check_status "${sourceanalyzer}" @"${buildFile}"
-    run_and_check_status "${sourceanalyzer}" -b "${buildId}" -show-build-warnings > "${logDir}${buildId}-buildWarnings.log"
-    run_and_check_status "${sourceanalyzer}" -b "${buildId}" -show-files > "${logDir}${buildId}-showfiles.log"
-    run_and_check_status "${sourceanalyzer}" @"${scanFile}"
+    run_and_check_status "${sourceanalyzer}" -b "${buildId}" "${sourceDir}" -logfile "${logDir}${buildId}-build.log" "${fortifyDebug}" -autoheap
+
+    echo
+    echo -n `date "+%D %H:%M:%S"`
+    echo " : Writing warnings to ${logDir}${buildId}-buildWarnings.log"
+    echo
+
+    # Capture build warnings and show the files in the build
+    run_and_check_status "${sourceanalyzer}" -b "${buildId}" -show-build-warnings -logfile "${logDir}${buildId}-buildWarnings.log" "${fortifyDebug}"
+
+    echo
+    echo -n `date "+%D %H:%M:%S"`
+    echo " : Listing build files to ${logDir}${buildId}-showfiles.log"
+    echo
+
+    run_and_check_status "${sourceanalyzer}" -b "${buildId}" -show-files -logfile "${logDir}${buildId}-showfiles.log" "${fortifyDebug}"
+
+    echo
+    echo -n `date "+%D %H:%M:%S"`
+    echo " : Starting scan"
+    echo
+
+    # Perform scan
+    run_and_check_status "${sourceanalyzer}" -b "${buildId}" -scan -f "${reportDir}${buildId}-scan.fpr" -logfile "${logDir}${buildId}-scan.log" "${fortifyDebug}" -autoheap
+
+    echo
+    echo -n `date "+%D %H:%M:%S"`
+    echo " : Generating report ${reportDir}${buildId}-results.pdf"
+    echo
+
+    # Generate report from scan results
     run_and_check_status "${ReportGenerator}" -format "pdf" -f "${reportDir}${buildId}-results.pdf" -source "${reportDir}${buildId}-scan.fpr"
 
-    cp "${scanFile}" "${logDir}${scanFile}.log"
-    cp "${buildFile}" "${logDir}${buildFile}.log" 
-    
+    echo
     echo -n `date "+%D %H:%M:%S"`
-    echo " : Successfully completed fortify scan"
+    echo " : Successfully completed fortify test"
 }
 
-fortify > "${logDir}run.log"
+fortify > "${reportDir}${buildId}-run.log"
